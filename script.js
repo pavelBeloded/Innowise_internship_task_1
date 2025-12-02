bdf0e2c241b191955784ae52e888f07ba5c9503a
PavelBeloded <beloded_2007@bk.ru>
2025-12-03 00:14:03 +0300

    feat: burger menu created

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const header = document.querySelector(".header");
+    burger.addEventListener("click", ()=> {
+        header.classList.toggle("open")
+    
+    })
+})
